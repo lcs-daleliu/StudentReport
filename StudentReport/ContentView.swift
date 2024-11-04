a2f7969295e48c079f327ad1a10ce76c43cd840d
Dale Liu <YLiu@lcs.on.ca>
2024-11-04 09:32:32 -0500

    Create a couple of charts to visualize the grades of my subjects

--- StudentReport/ContentView.swift
+++ StudentReport/ContentView.swift
@@ -6,16 +6,23 @@
 //
 
 import SwiftUI
+import Charts
 
 struct ContentView: View {
+    let subjects = [ "English", "Functions", "Caculus", "Computer Science", "Chemistry", "Economy", "Physics" ]
+    let steps = [ 77, 98, 95, 90, 97, 95, 96 ]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Chart {
+            ForEach(subjects.indices, id: \.self) { index in
+                BarMark(x: .value("Grade", subjects[index]), y: .value("Steps", steps[index]))
+                    .foregroundStyle(by: .value("Grade", subjects[index]))
+                    .annotation {
+                        Text("\(steps[index])")
+                    }
+            }
         }
-        .padding()
+        .padding(50)
     }
 }
 
