acb328c2a8f75d1c3f9a027173fc7f83094d9128
Dale Liu <YLiu@lcs.on.ca>
2024-11-04 08:57:05 -0500

    Initial Commit

---
+++ StudentReport/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StudentReport
+//
+//  Created by Yiren LIU on 2024-11-04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StudentReport/StudentReportApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudentReportApp.swift
+//  StudentReport
+//
+//  Created by Yiren LIU on 2024-11-04.
+//
+
+import SwiftUI
+
+@main
+struct StudentReportApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
